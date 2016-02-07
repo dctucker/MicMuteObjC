2807a6afcade9c746eb3bd64ced0942dffb15d78
Casey Tucker <casey.tucker@brax.io>
2016-02-06 18:17:21 -0800

    Initial Commit

---
+++ MicMuteObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MicMuteObjC
+//
+//  Created by Douglas Casey Tucker on 2016-02-06.
+//  Copyright © 2016 Douglas Casey Tucker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
